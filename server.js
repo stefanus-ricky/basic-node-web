57fc976de58798a7129540afe3620a51c4456e24
srickyr <srickyriady@gmail.com>
2020-09-01 17:41:28 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+let count=1;
+console.log(__dirname);
+
+//app.use(express.static(__dirname + '/public'));
+
+http.createServer(function (req, res) {
+  let q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  if(q.pathname==="/"){
+    q.pathname+="index.html";
+  }
+  function get404(){
+    console.log(`can't find the url, sending 404`);
+    filename= "./404.html";
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    // trying to acess 404.html
+    fs.readFile(filename, function(err2, data2) {
+      if(err2){
+        console.log(`error at 404.html`);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+        console.log(`checkpoint1`)    
+        res.writeHead(302, {'Content-Type': 'text/html', 'Location':'./404.html'});
+        console.log(`write head success`);    
+
+        res.write(data2);
+        console.log(`write data`)    
+        return res.end();
+      })
+  }
+
+  console.log(`${count}. q.pathname is ${q.pathname} type ${typeof q.pathname}`);
+  count++;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      return get404();
+      console.log(`checkpoint2`);    
+    } 
+    // if file found
+    console.log(`checkpoint3`)    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
+
+/*
+
+      fs.readFile(filename, function(err2, data2) {
+        if(err2){
+            console.log(`error at 404.html`)
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+            console.log(`checkpoint1`)    
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(data2)
+            return res.end();
+        })
+*/
